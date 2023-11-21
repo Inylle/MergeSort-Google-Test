301153cf897dc91b576d9bb82ee3d8b5027a8485
Jakub Piwko <jakub.piwko@op.pl>
2023-11-21 23:57:15 +0100

    MergeSort | Reverse Sorted Array

--- MergeSortTEST/TESTS/test.cpp
+++ MergeSortTEST/TESTS/test.cpp
@@ -3,12 +3,18 @@
 
 //vector<int> array = { 1, 5, 15, 97, 20, 6, 4, 16, 54 };
 MergeSort m1;
-vector<int> arr_check = { 2, 5, 7, 9, 13, 18, 20 };
+vector<int> arr_check = { 4, 6, 8, 11, 12, 17, 22 };
 
 vector<int> arr = {};
 
-TEST(Merge_sort_test, TestAlreadySortedArr) {
-    arr = { 2, 5, 7, 9, 13, 18, 20 };
+TEST(Merge_sort_test, TestAlreadySortedArray) {
+    arr = { 4, 6, 8, 11, 12, 17, 22 };
+    m1.sort(arr);
+    EXPECT_EQ(arr, arr_check);
+}
+
+TEST(Merge_sort_test, TestReverseSortedArray) {
+    arr = { 22, 17, 12, 11, 8, 6, 4 };
     m1.sort(arr);
     EXPECT_EQ(arr, arr_check);
 }
